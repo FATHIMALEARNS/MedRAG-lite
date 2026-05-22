532f5580b6ed285acaf8a2d60a82b9343fa5b84e
FATHIMALEARNS <fathimasiyad94@gmail.com>
2026-05-22 12:21:34 +0530

    chore: remove test and temporary files for clean deployment

--- test_retriever.py
+++
@@ -1,9 +0,0 @@
-from retriever import SimpleRetriever
-
-retriever = SimpleRetriever()
-
-query = "What does an abnormal chest X-ray indicate?"
-context = retriever.retrieve(query)
-
-print("\nRetrieved context:\n")
-print(context)
